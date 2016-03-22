534593df05f41900da683cc980c180cf1f5b5eb8
Shayon Mukherjee <dev@shayon.me>
2016-03-22 09:26:05 -0500

    Some minor refactorings were in order

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,20 +1,15 @@
 class Todo < ActiveRecord::Base
-  after_update :todo_complete_if_sub_tasks_complete, if: ->(todo){ todo.type == "SubTask" }
-  after_update :sub_tasks_complete_if_task_complete, if: ->(todo){ todo.type != "SubTask" }
-  after_update :mass_update_sub_tasks_if_task_complete, if: ->(todo){ todo.type != "SubTask" }
-
   scope :completed, -> { where("completed = ?", true) }
   scope :active, -> { where("completed = ?", false) }
   scope :main_tasks, -> { where(parent_id: nil) }
 
   has_many :sub_tasks, foreign_key: :parent_id, dependent: :destroy
 
+  after_update :sub_tasks_complete_if_task_complete, if: ->(todo){ todo.type != "SubTask" }
+  after_update :mass_update_sub_tasks_if_task_complete, if: ->(todo){ todo.type != "SubTask" }
+
   def title=(title)
     write_attribute(:title, title.strip)
-  end
-
-  def todo_complete_if_sub_tasks_complete
-    todo.update_attribute(:completed, true) if todo.sub_tasks.count == todo.sub_tasks.completed.count
   end
 
   def sub_tasks_complete_if_task_complete
